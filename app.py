a60d86ead574938f18c9c4796cb49ba1d8476edb
pradeepodela <72432569+pradeepodela@users.noreply.github.com>
2022-06-02 11:29:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from branch1 import *
+app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return chatbot_response(userText)
+
+
+if __name__ == "__main__":
+    app.run()
